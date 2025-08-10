cb3f48d58c5b066d7a7e53dfd9ad1190d374c43f
amaso <amasoufiane@gmail.com>
2025-08-10 18:48:36 -0400

    spring init

---
+++ src/main/java/com/example/testmodelpasse/TestmodelpasseApplication.java
@@ -0,0 +1,13 @@
+package com.example.testmodelpasse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestmodelpasseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestmodelpasseApplication.class, args);
+	}
+
+}
